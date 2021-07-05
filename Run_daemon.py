59e27048ea61332be34d1500c4e3bc9b97ab25cf
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-07-05 23:26:58 +0200

    [ref] Demonized the execution of the server code

--- Run_daemon.py
+++ Run_daemon.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 from Remote_control import Daemon_run
+import daemon
+from os.path import dirname, abspath
 
 if __name__ == "__main__":
-  # with daemon.DaemonContext(working_directory=dirname(abspath(__file__))):
-  Daemon_run(1148)()
+  with daemon.DaemonContext(working_directory=dirname(abspath(__file__))):
+    Daemon_run(1148)()
