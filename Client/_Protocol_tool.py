9f194950bd62ebf4983364dce7bcc6c2c1ca3709
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-07-05 16:23:28 +0200

    [ref] Added __init__.py files so that the folders are considered as packages

--- Client/_Protocol_tool.py
+++ Client/_Protocol_tool.py
@@ -131,6 +131,10 @@ class Stimulation_protocol:
 
         if not os.path.exists(path + "/Protocols/"):
           os.mkdir(path + "/Protocols/")
+          with open(path + "/Protocols/" + "__init__.py", 'w') as init_file:
+            init_file.write("# coding: utf-8" + "\n")
+            init_file.write("\n")
+            init_file.write("pass" + "\n")
 
         with open(path + "/Create_protocol.py", 'r') as file:
           with open(path + "/Protocols/" + "Protocol_" + name + ".py", 'w') \
