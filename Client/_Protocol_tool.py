a3996772a24ec683a81dc513128cf7cefde76fcd
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-07-05 15:17:02 +0200

    [ref] Modified the management of newly created protocols
    Also split Protocol_tools.py to a tool file and an executable file

--- Client/_Protocol_tool.py
+++ Client/_Protocol_tool.py
@@ -23,7 +23,7 @@ class Stimulation_protocol:
 
     self._py_file = ["# coding: utf-8" + "\n",
                      "\n",
-                     "from Protocol_tools import Stimulation_protocol" + "\n",
+                     "from .._Protocol_tool import Stimulation_protocol" + "\n",
                      "\n",
                      ]
 
@@ -128,10 +128,18 @@ class Stimulation_protocol:
       if answer in ["Yes", "yes"]:
 
         path = os.path.dirname(os.path.abspath(__file__))
+
         if not os.path.exists(path + "/Protocols/"):
           os.mkdir(path + "/Protocols/")
-        with open(__file__, 'r') as file:
-          with open(path + "/Protocols/" + name + ".py", 'w') as exported_file:
+          with open(path + "/Protocols/" + "__init__.py", 'w') as init_file:
+            init_file.write("# coding: utf-8" + "\n")
+            init_file.write("\n")
+            init_file.write("from .Protocol_" + name +
+                            " import Led, Mecha, Elec" + "\n")
+
+        with open(path + "/Create_protocol.py", 'r') as file:
+          with open(path + "/Protocols/" + "Protocol_" + name + ".py", 'w') \
+                    as exported_file:
 
             for line in self._py_file:
               exported_file.write(line)
@@ -367,49 +375,3 @@ class Stimulation_protocol:
     plt.plot(is_active_graph[0], is_active_graph[1])
 
     plt.show()
-
-
-if __name__ == '__main__':
-
-  new_prot = Stimulation_protocol()
-
-  # First 24 hours resting phase
-  new_prot.add_mechanical_rest(resting_position_mm=0,
-                               rest_duration_hours=24)
-
-  new_prot.add_electrical_rest(rest_duration_hours=24)
-
-  # Continuous stretching only
-  new_prot.add_continuous_stretching(travel_length_mm=2,
-                                     resting_time_ratio=0.25,
-                                     consecutive_stretch_duration_hours=1.5,
-                                     total_duration_hours=4 * 24)
-
-  new_prot.add_electrical_rest(rest_duration_hours=4 * 24)
-
-  # 3 days resting phase
-  new_prot.add_electrical_rest(rest_duration_hours=3 * 24)
-
-  new_prot.add_mechanical_rest(rest_duration_hours=3 * 24,
-                               resting_position_mm=2)
-
-  # Beginning electrical stimulation and slowly increasing mechanical
-  # stimulation
-  for n in range(2 * 24 * 8):
-    new_prot.add_cyclic_stretching(resting_position_mm=2,
-                                   stretched_position_mm=2 + 3 * (n + 1) /
-                                   (2 * 24 * 8),
-                                   number_of_cycles=5,
-                                   number_of_reps=3,
-                                   time_to_reach_position_seconds=2,
-                                   number_of_sets=1,
-                                   rest_between_sets_minutes=28,
-                                   rest_between_cycles_minutes=0.5)
-
-  new_prot.add_electrical_stimulation(pulse_duration_seconds=0.01,
-                                      delay_between_pulses_seconds=1,
-                                      set_duration_minutes=60,
-                                      rest_between_sets_minutes=5 * 60,
-                                      number_of_sets=4 * 8)
-
-  new_prot.save_protocol("b")
