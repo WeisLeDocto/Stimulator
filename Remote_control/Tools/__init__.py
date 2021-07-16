02e231d38b32e54e10bb3b56c9a1982bd24c92fc
Antoine Weisrock <antoine.weisrock@gmail.com>
2021-07-16 13:59:23 +0200

    [ref] Made the imports optional in Tools/ so that the server can run without it installed

--- Remote_control/Tools/__init__.py
+++ Remote_control/Tools/__init__.py
@@ -1,4 +1,7 @@
 # coding: utf-8
 
 from ._Protocol_phases import Protocol_phases, Protocol_parameters
-from ._Protocol_builder import Protocol_builder
+try:
+  from ._Protocol_builder import Protocol_builder
+except ModuleNotFoundError:
+  pass
