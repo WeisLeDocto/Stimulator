d2732b81b28790d910b6fbf8e9aea5a8ddb411ac
Antoine Weisrock <antoine.weisrock@gmail.com>
2022-05-03 16:26:53 +0200

    [ref] Refactored the Daemon class for driving the server

--- Remote_control/Server/_Daemon.py
+++ Remote_control/Server/_Daemon.py
@@ -14,8 +14,6 @@ from psutil import Process, AccessDenied
 class DaemonStop(Exception):
   """Exception raised for stopping the daemon thread."""
 
-  pass
-
 
 class Daemon_run:
   """A class for managing the stimulation protocols.
@@ -28,7 +26,7 @@ class Daemon_run:
 
   def __init__(self,
                port: int,
-               broker: bool,
+               manage_broker: bool,
                address: str = 'localhost',
                topic_in: str = 'Remote_control',
                topic_out: str = 'Server_status',
@@ -39,7 +37,7 @@ class Daemon_run:
 
     Args:
       port: The network port the broker should listen to.
-      broker: If :obj:`True`, starts the Mosquitto Broker.
+      manage_broker: If :obj:`True`, starts the Mosquitto Broker.
       address: The network address of the broker.
       topic_in: The topic for receiving commands from clients.
       topic_out: The topic for sending messages to the clients.
@@ -51,7 +49,7 @@ class Daemon_run:
 
     if not isinstance(port, int):
       raise TypeError("port should be an integer")
-    if not isinstance(broker, bool):
+    if not isinstance(manage_broker, bool):
       raise TypeError("broker should be a bool")
     if not isinstance(topic_in, str):
       raise TypeError("topic_in should be a string")
@@ -64,7 +62,7 @@ class Daemon_run:
     if not isinstance(topic_protocol_list, str):
       raise TypeError("topic_protocol_list should be a string")
 
-    self._broker = broker
+    self._manage_broker = manage_broker
 
     # Setting the topics and the queue
     self._topic_in = topic_in
@@ -81,13 +79,14 @@ class Daemon_run:
     self._client.on_message = self._on_message
     self._client.reconnect_delay_set(max_delay=10)
 
-    # Setting the flags
+    self._base_path = Path(__file__).parent.parent
+    self._protocols_path = self._base_path / 'Protocols'
+    self._protocol_path = self._base_path.parent / "Protocol.py"
+
     self._protocol = None
-    self._is_protocol_active = False
-    self._last_return_code = None
 
     # Starting the mosquitto broker
-    if self._broker:
+    if self._manage_broker:
       self._launch_mosquitto(port)
       sleep(5)
 
@@ -126,7 +125,7 @@ class Daemon_run:
       print("Finishing")
       self._client.loop_stop()
       self._client.disconnect()
-      if self._broker:
+      if self._manage_broker:
         try:
           self._mosquitto.terminate()
           self._mosquitto.wait(timeout=15)
@@ -202,18 +201,12 @@ class Daemon_run:
     """
 
     while True:
-      # Checking if the protocol is still running
-      if self._is_protocol_active:
-        if self._protocol.poll() is not None:
-          self._is_protocol_active = False
-          self._last_return_code = self._protocol.poll()
-
       # Getting the command message and executing the associated action
       if not self._message_queue.empty():
         try:
           message = self._message_queue.get_nowait()
         except Empty:
-          message = None
+          continue
 
         if message == "Return protocol list":
           print("Return protocol list")
@@ -221,18 +214,18 @@ class Daemon_run:
 
         elif message == "Print status":
           print("Print status")
-          self._protocol_status()
+          self._send_protocol_status()
 
-        elif "Upload protocol" in message:
+        elif message.startswith("Upload protocol"):
           print("Save protocol")
           self._save_protocol(*message.replace("Upload protocol ", "").
                               split(" "))
 
-        elif "Download protocol" in message:
+        elif message.startswith("Download protocol"):
           print("Send protocol")
           self._send_protocol(message.replace("Download protocol ", ""))
 
-        elif "Start protocol" in message:
+        elif message.startswith("Start protocol"):
           print("Start protocol")
           self._start_protocol(message.replace("Start protocol ", ""))
 
@@ -253,24 +246,23 @@ class Daemon_run:
 
       sleep(1)
 
-  def _protocol_status(self) -> None:
+  def _send_protocol_status(self) -> None:
     """Sends the protocol status to the clients."""
 
-    if self._is_protocol_active:
-      self._publish("Protocol running")
-    elif self._last_return_code == 0:
-      self._publish("Protocol terminated gracefully")
-    elif self._last_return_code is not None:
-      self._publish("Protocol terminated with an error")
-    else:
+    if self._protocol is None:
       self._publish("No protocol started yet")
+    elif self._is_protocol_active:
+      self._publish("Protocol running")
+    elif self._protocol.poll() == 0:
+      self._publish("Last protocol terminated gracefully")
+    else:
+      self._publish("Last protocol terminated with an error")
 
   def _send_protocol_list(self) -> None:
     """Sends the clients the list of protocols in the Protocols/ folder"""
 
     try:
-      path = Path(__file__).parent.parent
-      protocol_list = Path.iterdir(path / "Protocols")
+      protocol_list = Path.iterdir(self._protocols_path)
       protocols = [protocol.name.replace("Protocol_", "").replace(".py", "")
                    for protocol in protocol_list if
                    protocol.name.startswith("Protocol")]
@@ -291,19 +283,16 @@ class Daemon_run:
       name: The name of the protocol to send.
     """
 
-    protocol = []
-    path = Path(__file__).parent.parent
-    with open(path / "Protocols" / ("Protocol_" + name + ".py"), 'r') \
-         as protocol_file:
-      for line in protocol_file:
-        protocol.append(line)
+    with open(self._protocols_path / ("Protocol_" + name + ".py"),
+              'r') as protocol_file:
+      protocol = list(protocol_file)
 
     if self._client.publish(topic=self._topic_protocol_out,
                             payload=dumps(protocol),
-                            qos=2)[0]:
-      self._publish("Error ! Protocol not properly sent")
-    else:
+                            qos=2).is_published():
       self._publish("Protocol successfully downloaded")
+    else:
+      self._publish("Error ! Protocol not properly sent")
 
   def _save_protocol(self, name: str, p_word: str) -> None:
     """Saves a protocol uploaded by a client in the Protocols/ folder.
@@ -317,8 +306,7 @@ class Daemon_run:
         server.
     """
 
-    path = Path(__file__).parent.parent
-    with open(path / "password.txt", 'r') as password_file:
+    with open(self._base_path / "password.txt", 'r') as password_file:
       password = password_file.read()
     if p_word != password:
       self._publish("Error ! Wrong password")
@@ -327,24 +315,25 @@ class Daemon_run:
     try:
       protocol = self._protocol_queue.get(timeout=5)
 
-      if not Path.exists(path / "Protocols"):
-        Path.mkdir(path / "Protocols")
-        with open(path / "Protocols" / "__init__.py", 'w') as init_file:
+      if not Path.exists(self._protocols_path):
+        Path.mkdir(self._protocols_path)
+
+        with open(self._protocols_path / "__init__.py", 'w') as init_file:
           init_file.write("# coding: utf-8" + "\n")
           init_file.write("\n")
           init_file.write("from .Protocol_" + name + " import Led, Mecha, Elec"
                           + "\n")
 
-      with open(path / "Protocols" / ("Protocol_" + name + ".py"), 'w') as \
-           protocol_file:
+      with open(self._protocols_path / ("Protocol_" + name + ".py"),
+                'w') as protocol_file:
         for line in protocol:
           protocol_file.write(line)
       self._publish("Protocol successfully uploaded")
+
     except Empty:
       self._publish("Error ! No protocol received")
 
-  @staticmethod
-  def _choose_protocol(protocol: str) -> None:
+  def _choose_protocol(self, protocol: str) -> None:
     """Chooses the right protocol to start.
 
     Overwrites the ``__init__.py`` file in the Protocols/ folder so that the
@@ -354,21 +343,18 @@ class Daemon_run:
       protocol: The name of the protocol to choose.
     """
 
-    path = Path(__file__).parent.parent
-    with open(path / "Protocols" / "__init__.py", 'w') as init_file:
+    with open(self._protocols_path / "__init__.py", 'w') as init_file:
       init_file.write("# coding: utf-8" + "\n")
       init_file.write("\n")
       init_file.write("from .Protocol_" + protocol + " import Led, Mecha, Elec"
                       + "\n")
 
-  @ staticmethod
-  def _write_protocol():
+  def _write_protocol(self):
     """Writes the ``Protocol.py`` file using the generator lists and the
     ``_Protocol_template.py`` file."""
 
     from ..Protocols import Led, Mecha, Elec
-    path = Path(__file__).parent.parent.parent
-    with open(path / "Protocol.py", 'w') as executable_file:
+    with open(self._protocol_path, 'w') as executable_file:
       executable_file.write('# coding: utf-8' + "\n")
       executable_file.write("\n")
 
@@ -387,7 +373,7 @@ class Daemon_run:
         executable_file.write(str(dic) + "," + "\n")
       executable_file.write("]" + "\n")
 
-      with open(path / "Remote_control" / "Server" / "_Protocol_template.py",
+      with open(self._base_path / "Server" / "_Protocol_template.py",
                 'r') as template:
         for line in template:
           if "#" not in line:
@@ -407,18 +393,17 @@ class Daemon_run:
     if not self._is_protocol_active:
       self._choose_protocol(protocol)
       self._write_protocol()
-      self._protocol = Popen(['python3', 'Protocol.py'])
+      self._protocol = Popen(['python3', self._protocol_path])
       try:
         self._protocol.wait(5)
         try:
           self._protocol.wait(5)
           self._publish("Error ! Protocol crashed at starting")
         except TimeoutExpired:
-          self._is_protocol_active = True
           self._publish("Protocol started")
       except TimeoutExpired:
-        self._is_protocol_active = True
         self._publish("Protocol started")
+
     else:
       self._publish("Protocol already running, stop it before starting new "
                     "one")
@@ -437,33 +422,38 @@ class Daemon_run:
     if not self._is_protocol_active:
       print("No protocol currently running !")
       self._publish("No protocol currently running !")
+
     else:
       # Trying to get the protocol to stop itself
       self._protocol.send_signal(SIGINT)
       try:
-        self._last_return_code = self._protocol.wait(10)
+        self._protocol.wait(10)
       except TimeoutExpired:
         try:
           # If not successful, asking the process to stop
           self._protocol.terminate()
-          self._last_return_code = self._protocol.wait(10)
+          self._protocol.wait(10)
         except TimeoutExpired:
           # If still not successful, abruptly killing the process
           self._protocol.kill()
-          self._last_return_code = self._protocol.wait(10)
+          self._protocol.wait(10)
 
       # Sending the results to the clients
-      if self._last_return_code is None:
+      if self._protocol.poll() is None:
         print("Error ! Could not stop the protocol")
         self._publish("Error ! Could not stop the protocol")
-        self._is_protocol_active = True
         return 1
-      elif self._last_return_code == 0:
+      elif self._protocol.poll() == 0:
         print("Protocol terminated gracefully")
         self._publish("Protocol terminated gracefully")
-        self._is_protocol_active = False
       else:
         print("Protocol terminated with an error")
         self._publish("Protocol terminated with an error")
-        self._is_protocol_active = False
+
     return 0
+
+  @property
+  def _is_protocol_active(self) -> bool:
+    """Returns True is there is currently an active protocol running."""
+
+    return self._protocol is not None and self._protocol.poll() is None
